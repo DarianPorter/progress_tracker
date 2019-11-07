954995219683068ad8fd0da42e8314a12362af23
darianporter <darianporter@icloud.com>
2019-11-06 22:17:47 -0500

    admin new student form

--- frontend/components/admin/new_student_form.jsx
+++ frontend/components/admin/new_student_form.jsx
@@ -14,12 +14,32 @@ class SignUpForm extends React.Component{
     }
 
     feildsComplete(){
-
+        for(let key in this.state){
+            if (this.state[key].length == 0){
+                return false
+            }
+        }
+        return true
     }
 
+    setStateVals(key, e){
+        debugger
+        this.setState({[key]: e.target.value})
+        if(key == "last_name"){
+            this.setState({ password: e.target.value + this.state.class_year })
+        }else if (key == "class_year"){
+            this.setState({ password: this.state.last_name + e.target.value })
+        }
+    }
+
+    submitNewStudent(){
+        debugger
+        
+    }
+    
     render(){
         let button = this.feildsComplete() ? (
-            <button>
+            <button onClick={()=>{this.submitNewStudent}} className="sign-up-student-button">
                 Submit
             </button>
         ) : (
@@ -33,20 +53,34 @@ class SignUpForm extends React.Component{
                 <div className="sign-up-form-name">
                     <div>        
                         <h2>Firstname:</h2>
-                        <input type="text" placeholder="First Name"/>
+                        <input onChange={(e)=>{
+                            this.setStateVals("first_name", e)
+                        }} type="text" placeholder="First Name"/>
                     </div>  
                     <div>
                         <h2>Lastname:</h2>
-                        <input type="text" placeholder="Last Name"/>
+                        <input onChange={(e) => {
+                            this.setStateVals("last_name", e)
+                        }} type="text" placeholder="Last Name"/>
                     </div>  
                 </div>
                 <div className="sign-up-form-email">
                     <h2>Email</h2>
-                    <input type="text" placeholder="Email"/>
+                    <input onChange={(e) => {
+                        this.setStateVals("email", e)
+                    }} type="text" placeholder="Email"/>
                 </div>
-                <div className="sign-up-form-password">
-
-                    <input type="number" min={2016}/>
+                <div className="sign-up-form-class-year-password">
+                    <div>
+                        <h2> Password: Lastname + Class Year </h2>
+                        <p className="highlight"> { this.state.password } </p>
+                    </div>
+                    <div>
+                        <h2> Class Year: </h2>
+                        <input onChange={(e) => {
+                            this.setStateVals("class_year", e)
+                        }}type="number" min={2016} placeholder="Year"/>
+                    </div>
                 </div>
                 <div>
                     {button}
